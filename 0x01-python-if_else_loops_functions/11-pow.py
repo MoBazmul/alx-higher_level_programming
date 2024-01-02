a138b6545f904070c96c57a2c78f08588e4c3a9e
MoBazmul <mohaarash001@gmail.com>
2024-01-02 04:06:31 -0800

    All uploads

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# Author - Mohammed Rashid
+
+def pow(a, b):
+    return (a ** b)
+
---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# Author - MOhammed Rashid
+
+def print_last_digit(number):
+    print(abs(number) % 10, end="")
+    return (abs(number) % 10)
+
