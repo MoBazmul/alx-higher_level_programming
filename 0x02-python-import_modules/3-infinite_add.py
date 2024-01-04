bf65790bc14f5f59091ca1e3ab128d90361ba9f4
MoBazmul <mohaarash001@gmail.com>
2024-01-04 02:41:40 -0800

    Adding the condition to check for the length of the arguments passed

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -5,7 +5,10 @@ if __name__ == "__main__":
     import sys
 
     total = 0
-    for i in range(len(sys.argv) - 1):
-        total += int(sys.argv[i + 1])
+    if len(sys.argv) < 2:
+        total = 0
+    else:
+        for arg in sys.argv[1:]:
+            total += int(arg)
     print("{}".format(total))
 
